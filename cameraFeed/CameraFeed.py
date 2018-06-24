37cd286a3546e3666e9814377b6574160fd802aa
lorihompoth <lorihompoth@gmail.com>
2018-06-24 21:49:44 +0300

    camera producer consumer

--- cameraFeed/CameraFeed.py
+++ cameraFeed/CameraFeed.py
@@ -1,23 +1,16 @@
 import threading
-from cameraFeed.PrepareFrameThread import PrepareFrameThread
+from cameraFeed.Camera import Camera
+
 
 
 class CameraFeed:
-    def __init__(self, camera, xFlip, yFlip):
-        self.__camera = camera
-        self.__xFlip = xFlip
-        self.__yFlip = yFlip
-        #self.__prepareThread = PrepareFrameThread(self.__camera, self.__xFlip, self.__yFlip)
-        #self.__prepareThread.start()
-    '''
-    def getFrame(self):
-        self.__prepareThread.join()
-        frame = self.__prepareThread.getPreparedFrame()
-        self.__prepareThread = PrepareFrameThread(self.__camera, self.__xFlip, self.__yFlip)
-        self.__prepareThread.start()
-        return frame
-    '''
-    def getFrame(self):
-        return self.__camera.getFrame()
+    def __init__(self, width, height, rotation):
+        self.__cv = threading.Condition()
+        self.__camera = Camera(width, height, rotation, self.__cv)
 
+    def getFrame(self):
+        with self.__cv:
+            while not self.__cam.isAvailable():
+                self.__cv.wait()
+        return self.__cam.getSavedFrame()
 
