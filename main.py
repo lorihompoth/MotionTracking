1ccdd3c35a511273cf4bc7d75d06a177395bbe25
lorihompoth <lorihompoth@gmail.com>
2018-06-24 16:16:17 +0300

    prints in main added

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ from motionTracking.MotionTracking import MotionTracking
 from cameraFeed.Camera import Camera
 from cameraFeed.CameraFeed import CameraFeed
 
-print ("started")
+print("started")
 
-#heyasdasdsdfsf
 WIDTH = 432
 HEIGHT = 368
 FRAMERATE = 20
+print("camera()")
 cam = Camera(WIDTH, HEIGHT, FRAMERATE)
+print("camera constructed")
+print("cameraFeed()")
 cf = CameraFeed(cam, True, True)
+print("cameraFeed constructed")
+print("motionTracking()")
 mt = MotionTracking(cf)
+print("motionTracking constructed")
 while True:
+	print("motionTracking.getFinal() called")
 	image = mt.getFinal()
+	print("motionTracking.getFinal() finished")
 	cv2.imshow("asd", image)
+	print("imshow finished")
