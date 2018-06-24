0d713b9824a237b52e8cc60e97ecb169766c3b12
lorihompoth <lorihompoth@gmail.com>
2018-06-24 17:50:45 +0300

    camera truncate from second frame onwards

--- cameraFeed/Camera.py
+++ cameraFeed/Camera.py
@@ -14,13 +14,14 @@ class Camera:
         self.__cam.resolution = (width, height)
         self.__cam.framerate = framerate
         self.__rawCapture = PiRGBArray(self.__cam, size=(self.__width, self.__height))
-
+        self.__sentAFrame = False
         # camera warmup:
         time.sleep(0.1)
 
     def getFrame(self):
         print("camera.truncate")
-        #self.__rawCapture.truncate(0)
+        if self.__sentAFrame:
+            self.__rawCapture.truncate(0)
         print("camera.for")
         for frame in self.__cam.capture_continuous(self.__rawCapture, format="bgr", use_video_port=True):
             #print("camera.show preview...")
