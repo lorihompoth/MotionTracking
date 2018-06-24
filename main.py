2aa1ce814f8cceae2b1360a2b9659dba0d1f9ed6
lorihompoth <lorihompoth@gmail.com>
2018-06-24 22:37:07 +0300

    asd

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from motionTracking.MotionTracking import MotionTracking
 from cameraFeed.Camera import Camera
 from cameraFeed.CameraFeed import CameraFeed
 import cv2
+import time
 
 print("started")
 
@@ -14,10 +15,13 @@ camFeed = CameraFeed(WIDTH, HEIGHT, ROTATION, FRAMERATE)
 print("cameraFeed constructed")
 mt = MotionTracking(camFeed)
 print("motionTracking constructed")
+t = time.time()
 while True:
 	#print("motionTracking.getFinal() called")
 	image = mt.getFinal()
-	print (i)
+	fps = 1/(time.time() - t)
+	print("fps: " + str(fps))
+	t = time.time()
 	#print("motionTracking.getFinal() finished")
 	#cv2.imshow("asd", image)
 	#print("imshow finished")
