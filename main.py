84639d28758df62bb8ee032b1c5bf6e7d33bfb3c
lorihompoth <lorihompoth@gmail.com>
2018-06-25 03:55:14 +0300

    asa

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from view.Gui import Gui
 Gui().run()
 
-
-'''
 from motionTracking.MotionTracking import MotionTracking
 from cameraFeed.Camera import Camera
 from cameraFeed.CameraFeed import CameraFeed
 from view.GUI import GUI
 import cv2
 import time
+
 print("started")
 WIDTH = 432
 HEIGHT = 368
@@ -21,13 +20,12 @@ mt = MotionTracking(camFeed)
 print("motionTracking constructed")
 t = time.time()
 while True:
-        image = mt.getFinal()
-        if image is not None:
-                
-                cv2.imshow("asd", image)
-        else:
-                print("Main Prevented None")
-        #fps = 1/(time.time() - t)
-        #print("fps: " + str(int(fps)))
-        t = time.time()
-'''
+    image = mt.getFinal()
+    if image is not None:
+
+        cv2.imshow("asd", image)
+    else:
+        print("Main Prevented None")
+    # fps = 1/(time.time() - t)
+    # print("fps: " + str(int(fps)))
+    t = time.time()
--- view/Gui.py
+++ view/Gui.py
@@ -3,13 +3,6 @@ import gi # for GUI
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-from motionTracking.MotionTracking import MotionTracking
-from cameraFeed.Camera import Camera
-from cameraFeed.CameraFeed import CameraFeed
-from view.Gui import Gui
-import cv2
-import time
-
 class Gui:
     def __init__(self):
         # def initializeGUI():
@@ -50,30 +43,6 @@ class Gui:
         self.__standbyBetweenMovements = builder.get_object("entry2").get_text()
         self.__cameraFieldOfView = builder.get_object("entry4").get_text()
         self.__minTrigger = builder.get_object("entry3").get_text()
-
-
-    def run(self):
-        print("started")
-        WIDTH = 432
-        HEIGHT = 368
-        FRAMERATE = 20
-        ROTATION = 180
-        print("cameraFeed()")
-        camFeed = CameraFeed(WIDTH, HEIGHT, ROTATION, FRAMERATE)
-        print("cameraFeed constructed")
-        mt = MotionTracking(camFeed)
-        print("motionTracking constructed")
-        t = time.time()
-        while True:
-            image = mt.getFinal()
-            if image is not None:
-
-                cv2.imshow("asd", image)
-            else:
-                print("Main Prevented None")
-            # fps = 1/(time.time() - t)
-            # print("fps: " + str(int(fps)))
-            t = time.time()
 
 
 
