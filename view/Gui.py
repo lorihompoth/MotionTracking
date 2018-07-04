2d76cc0f6ea6b89f62dae4433f670d70315cf01e
lorihompoth <lorihompoth@gmail.com>
2018-07-04 19:55:52 +0300

    Qt gui class

--- view/Gui.py
+++ view/Gui.py
@@ -1,68 +1,265 @@
-
+'''
 from motionTracking.MotionTracking import MotionTracking
 from cameraFeed.Camera import Camera
 from cameraFeed.CameraFeed import CameraFeed
 import cv2
 import time
-
-import gi # for GU
-
 from threading import Thread
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
-
+'''
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
 class Gui:
     def __init__(self):
-        # def initializeGUI():
-        self.__builder = Gtk.Builder()
-        self.__builder.add_from_file("gui.glade")
-        self.__builder.connect_signals(self)
-        self.__window = self.__builder.get_object("dialog1")
-        self.__window.show_all()
-        Gtk.main()
+        self.initializeGUI()
+        self.tabs.show()
+        sys.exit(self.app.exec_()) 
+        
+    def initializeGUI(self):
+        self.app = QApplication(sys.argv)
+        self.tabs = QTabWidget()
+        # Create tabs
+        self.tab1    = QWidget()    
+        self.tab2    = QWidget()
+        self.tab3    = QWidget()
+        self.tab4    = QWidget()
+        
+        # Set Layout of Tab 1
+        self.vBoxLayout1 = QVBoxLayout()
+        self.checkBox1 = QCheckBox("Preview")
+        self.checkBox2 = QCheckBox("Final screen")
+        self.checkBox3 = QCheckBox("Phase 1 - Black and white")
+        self.checkBox4 = QCheckBox("Phase 2 - Difference")
+        self.checkBox5 = QCheckBox("Phase 3 - Binarize")
+        self.checkBox6 = QCheckBox("Phase 4 - Blur")
+        self.checkBox7 = QCheckBox("Phase 5 - Binarize")
+        
+        self.vBoxLayout1Sub = QVBoxLayout()
+        self.vBoxLayout1Sub.setContentsMargins(25, 0,0,0)
+        self.vBoxLayout1Sub.addWidget(self.checkBox2)
+        self.vBoxLayout1Sub.addWidget(self.checkBox3)
+        self.vBoxLayout1Sub.addWidget(self.checkBox4)
+        self.vBoxLayout1Sub.addWidget(self.checkBox5)
+        self.vBoxLayout1Sub.addWidget(self.checkBox6)
+        self.vBoxLayout1Sub.addWidget(self.checkBox7)
+        self.checkBox1.setChecked(False)
+        
+        self.vBoxLayout1.addWidget(self.checkBox1)
+        self.vBoxLayout1.addLayout(self.vBoxLayout1Sub)
+        self.tab1.setLayout(self.vBoxLayout1)   
+        
+        
+        # Set Layout of Tab 2
+        self.vBoxLayout2 = QVBoxLayout()
+        self.checkBox8 = QCheckBox("Record video")
+        self.checkBox8.setChecked(False)
+        self.label1 = QLabel("Destination folder: ")
+        
+        self.hBoxLayout1 = QHBoxLayout()
+        self.lineEdit1 = QLineEdit()
+        self.pushButton1 = QPushButton("Browse")
+        self.hBoxLayout1.addWidget(self.lineEdit1)
+        self.hBoxLayout1.addWidget(self.pushButton1)
+        
+        self.pushButton1.clicked.connect(selectFolderDialog)
+        self.checkBox9 = QCheckBox("Put timecode on video")
+        
+        self.hBoxLayout2 = QHBoxLayout()
+        self.label2 = QLabel("Font scale: ")
+        self.spinBox1 = QSpinBox()
+        self.spinBox1.setValue(1)
+        self.hBoxLayout2.addWidget(self.label2)
+        self.hBoxLayout2.addWidget(self.spinBox1)
+        
+        self.buttonGroup1 = QButtonGroup()
+        self.radioButton1 = QRadioButton("Record continuously")
+        self.radioButton2 = QRadioButton("Record movement only")
+        self.buttonGroup1.addButton(self.radioButton1)
+        self.buttonGroup1.addButton(self.radioButton2)
+        
+        self.buttonGroup2 = QButtonGroup()
+        self.radioButton3 = QRadioButton("Into a single file")
+        self.radioButton4 = QRadioButton("Separate files")
+        self.buttonGroup2.addButton(self.radioButton3)
+        self.buttonGroup2.addButton(self.radioButton4)
+        self.radioButton1.setChecked(True)
+        self.radioButton3.setChecked(True)
+        
+        
+        self.checkBox8.setContentsMargins(25, 0, 0, 0)
+        self.label1.setContentsMargins(25, 0, 0, 0)
+        self.hBoxLayout1.setContentsMargins(25, 0, 0, 0)
+        self.hBoxLayout2.setContentsMargins(25, 0, 0, 0)
+        self.vBoxLayout2.addWidget(self.checkBox8)
+        self.vBoxLayout2.addWidget(self.label1)
+        self.vBoxLayout2.addLayout(self.hBoxLayout1)
+        self.vBoxLayout2.addLayout(self.hBoxLayout2)
+        self.vBoxLayout2.addWidget(self.radioButton1)
+        self.vBoxLayout2.addWidget(self.radioButton2)
+        self.vBoxLayout2.addWidget(self.radioButton3)
+        self.vBoxLayout2.addWidget(self.radioButton4)
+        
+        
+        self.tab2.setLayout(self.vBoxLayout2)
+        
+        # Set Layout of Tab 3
+        self.vBoxLayout3 = QVBoxLayout()
+        
+        self.hBoxLayout3 = QHBoxLayout()
+        self.label3 = QLabel("Resolution: ")
+        self.comboBox1 = QComboBox()
+        self.comboBox1.addItem("432 x 368")
+        self.hBoxLayout3.addWidget(self.label3)
+        self.hBoxLayout3.addWidget(self.comboBox1)
+        
+        checkButton9 = QCheckBox("Aim towards motion")
+        checkButton10 = QCheckBox("Aim with arrow keys")
+        
+        self.hBoxLayout4 = QHBoxLayout()
+        self.label4 = QLabel("Blur value:")
+        self.lineEdit2 = QLineEdit()
+        self.lineEdit2.setValidator(QIntValidator())
+        self.lineEdit2.setText("40")
+        self.hBoxLayout4.addWidget(self.label4)
+        self.hBoxLayout4.addWidget(self.lineEdit2)
+        
+        self.hBoxLayout5 = QHBoxLayout()
+        self.label5 = QLabel("Noise threshold:")
+        self.lineEdit3 = QLineEdit()
+        self.lineEdit3.setValidator(QIntValidator())
+        self.lineEdit3.setText("20")
+        self.hBoxLayout5.addWidget(self.label5)
+        self.hBoxLayout5.addWidget(self.lineEdit3)
+        
+        self.hBoxLayout6 = QHBoxLayout()
+        self.label6 = QLabel("Standby between movements (ms):")
+        self.lineEdit4 = QLineEdit()
+        self.lineEdit4.setValidator(QIntValidator())
+        self.lineEdit4.setText("500")
+        self.hBoxLayout6.addWidget(self.label6)
+        self.hBoxLayout6.addWidget(self.lineEdit4)
+        
+        self.hBoxLayout7 = QHBoxLayout()
+        self.label7 = QLabel("Camera field of view (angle):")
+        self.lineEdit5 = QLineEdit()
+        self.lineEdit5.setValidator(QIntValidator())
+        self.lineEdit5.setText("180")
+        self.hBoxLayout7.addWidget(self.label7)
+        self.hBoxLayout7.addWidget(self.lineEdit5)
+        
+        self.hBoxLayout8 = QHBoxLayout()
+        self.label8 = QLabel("Movement trigger angular diff:")
+        self.lineEdit6 = QLineEdit()
+        self.lineEdit6.setValidator(QIntValidator())
+        self.lineEdit6.setText("75")
+        self.hBoxLayout8.addWidget(self.label8)
+        self.hBoxLayout8.addWidget(self.lineEdit6)
+        
+        self.vBoxLayout3.addLayout(self.hBoxLayout3)
+        self.vBoxLayout3.addLayout(self.hBoxLayout4)
+        self.vBoxLayout3.addLayout(self.hBoxLayout5)
+        self.vBoxLayout3.addLayout(self.hBoxLayout6)
+        self.vBoxLayout3.addLayout(self.hBoxLayout7)
+        self.vBoxLayout3.addLayout(self.hBoxLayout8)
+        self.tab3.setLayout(self.vBoxLayout3)
+        
+        # Add start and close buttons to all tabs
+        self.hBoxLayout9 = QHBoxLayout()
+        self.pushButton3 = QPushButton("Start application")
+        self.hBoxLayout9.setContentsMargins(0,15,0,0)
+        self.hBoxLayout9.addSpacerItem(QSpacerItem(100, 0, QSizePolicy.Maximum))
+        self.hBoxLayout9.addWidget(self.pushButton3)
+        self.hBoxLayout9.addSpacerItem(QSpacerItem(100, 0, QSizePolicy.Maximum))
+        
+        self.hBoxLayout10 = QHBoxLayout()
+        self.pushButton5 = QPushButton("Start application")
+        self.hBoxLayout10.setContentsMargins(0,15,0,0)
+        self.hBoxLayout10.addSpacerItem(QSpacerItem(100, 0, QSizePolicy.Maximum))
+        self.hBoxLayout10.addWidget(self.pushButton5)
+        self.hBoxLayout10.addSpacerItem(QSpacerItem(100, 0, QSizePolicy.Maximum))
+        
+        self.hBoxLayout11 = QHBoxLayout()
+        self.pushButton7 = QPushButton("Start application")
+        self.hBoxLayout11.setContentsMargins(0,15,0,0)
+        self.hBoxLayout11.addSpacerItem(QSpacerItem(100, 0, QSizePolicy.Maximum))
+        self.hBoxLayout11.addWidget(self.pushButton7)
+        self.hBoxLayout11.addSpacerItem(QSpacerItem(100, 0, QSizePolicy.Maximum))
+        
+        self.vBoxLayout1.addLayout(self.hBoxLayout9)
+        self.vBoxLayout2.addLayout(self.hBoxLayout10)
+        self.vBoxLayout3.addLayout(self.hBoxLayout11)
+        
+        # Add tabs
+        self.tabs.addTab(self.tab1,"Preview")
+        self.tabs.addTab(self.tab2,"Recording")
+        self.tabs.addTab(self.tab3,"Advanced")
+        
+        tabs.setWindowTitle('Motion Tracking')
 
-    def onDestroy(self, *args):
-        Gtk.main_quit()
+    def startButtonClicked(self):
+        self.__preview = None
+        self.__finalScreen = None
+        self.__phase1 = None
+        self.__phase2 = None
+        self.__phase3 = None
+        self.__phase4 = None
+        self.__phase5 = None
+        self.__recordVideo = None
+        self.__destinationFolder = None
+        self.__putTimecode = None
+        self.__fontSize = None
+        self.__recordContinously = None
+        self.__recordMovement = None
+        self.__intoASingleFile = None
+        self.__separateFiles = None
+        self.__resolution = None
+        self.__aimTowardsMotion = None
+        self.__aimWithArrowKeys = None
+        self.__blur = None
+        self.__threshold = None
+        self.__standbyBetweenMovements = None
+        self.__cameraFieldOfView = None
+        self.__minTrigger = None
+    
+    def togglePreviewEnabled():
+        newState = self.checkBox1.isChecked()
+        self.checkBox2.setEnabled(newState)
+        self.checkBox3.setEnabled(newState)
+        self.checkBox4.setEnabled(newState)
+        self.checkBox5.setEnabled(newState)
+        self.checkBox6.setEnabled(newState)
+        self.checkBox7.setEnabled(newState)
+    
+    def toggleSeparateFilesEnabled():
+        newState = self.radioButton2.isChecked()
+        self.radioButton3.setEnabled(newState)
+        self.radioButton4.setEnabled(newState)
+        
+    def toggleRecordEnabled():
+        newState = self.checkBox8.isChecked()
+        self.label1.setEnabled(newState)
+        self.label2.setEnabled(newState)
+        self.lineEdit1.setEnabled(newState)
+        self.pushButton1.setEnabled(newState)
+        self.spinBox1.setEnabled(newState)
+        self.radioButton1.setEnabled(newState)
+        self.radioButton2.setEnabled(newState)
+        self.radioButton3.setEnabled(newState)
+        self.radioButton4.setEnabled(newState)
+        toggleSeparateFilesEnabled()
 
-    def exitButton(self, button):
-        exit(0)
-
-    def on_button2_clicked(self, arg1):
-
-        self.__preview = self.__builder.get_object("checkbutton11").get_active()
-        self.__finalScreen = self.__builder.get_object("checkbutton1").get_active()
-        self.__phase1 = self.__builder.get_object("checkbutton2").get_active()
-        self.__phase2 = self.__builder.get_object("checkbutton3").get_active()
-        self.__phase3 = self.__builder.get_object("checkbutton4").get_active()
-        self.__phase4 = self.__builder.get_object("checkbutton5").get_active()
-        self.__phase5 = self.__builder.get_object("checkbutton6").get_active()
-        self.__recordVideo = self.__builder.get_object("checkbutton7").get_active()
-        self.__destinationFolder = self.__builder.get_object("filechooserbutton1").get_uri()
-        self.__putTimecode = self.__builder.get_object("checkbutton8").get_active()
-        self.__fontSize = self.__builder.get_object("entry5").get_text()
-        self.__recordContinously = self.__builder.get_object("radiobutton1").get_active()
-        self.__recordMovement = self.__builder.get_object("radiobutton2").get_active()
-        self.__intoASingleFile = self.__builder.get_object("radiobutton3").get_active()
-        self.__separateFiles = self.__builder.get_object("radiobutton4").get_active()
-        self.__resolution = self.__builder.get_object("comboboxtext1").get_active_text()
-        self.__aimTowardsMotion = self.__builder.get_object("checkbutton9").get_active()
-        self.__aimWithArrowKeys = self.__builder.get_object("checkbutton10").get_active()
-        self.__blur = self.__builder.get_object("entry1").get_text()
-        self.__threshold = self.__builder.get_object("entry6").get_text()
-        self.__standbyBetweenMovements = self.__builder.get_object("entry2").get_text()
-        self.__cameraFieldOfView = self.__builder.get_object("entry4").get_text()
-        self.__minTrigger = self.__builder.get_object("entry3").get_text()
-        #self.__window.destroy()
-        #self.__window.connect('delete-event', self.on_button2_clicked)
-        #Gtk.main_quit()
-        #t = Thread(target=self.runApp(), args=(i, ))
-        #t.start()
-        #t.join()
-        self.runApp()
+    def connectHandlers(self):
+        self.checkBox1.clicked.connect(togglePreviewEnabled)
+        self.checkBox8.clicked.connect(toggleRecordEnabled)
+        self.radioButton1.clicked.connect(toggleSeparateFilesEnabled)
+        self.radioButton2.clicked.connect(toggleSeparateFilesEnabled)
+        togglePreviewEnabled()
+        toggleRecordEnabled()
+        toggleSeparateFilesEnabled()
 
     def runApp(self):
-
+        '''
         WIDTH = 432
         HEIGHT = 368
         FRAMERATE = 20
@@ -78,6 +275,6 @@ class Gui:
                 fps = 1/(time.time() - t)
                 print("fps: " + str(int(fps)))
                 t = time.time()
-
+    '''
 
 
