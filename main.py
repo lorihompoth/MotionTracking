a38d461fcac6c1389560a178f0f9f428635f209d
lorihompoth <lorihompoth@gmail.com>
2018-07-05 12:04:19 +0300

    back to simple

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 import sys 
 
 if len(sys.argv) != 2:
@@ -10,38 +11,39 @@ elif sys.argv[1] == "0":
         pass
     print("gui terminated")
 elif sys.argv[1] == "1":
-    from motionTracking.MotionTracking import MotionTracking
-    from cameraFeed.Camera import Camera
-    from cameraFeed.CameraFeed import CameraFeed
-    import cv2
-    import time
-    import socket
-    
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(('localhost', 8089))
-    serversocket.listen(1)
-    
-    connection, address = serversocket.accept()
-    buf = connection.recv(4096)
-    if len(buf) > 0:
-        print buf
-    serversocket.close()
+'''
+from motionTracking.MotionTracking import MotionTracking
+from cameraFeed.Camera import Camera
+from cameraFeed.CameraFeed import CameraFeed
+import cv2
+import time
+import socket
 
-    #ast.literal_eval("{'1': 2}")
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('localhost', 8089))
+serversocket.listen(1)
 
-    WIDTH = 432
-    HEIGHT = 368
-    FRAMERATE = 20l
-    ROTATION = 180
-    camFeed = CameraFeed(WIDTH, HEIGHT, ROTATION, FRAMERATE)
-    #print(self.__destinationFolder[7:])
-    mt = MotionTracking(camFeed, None)
-    t = time.time()
-    while True:
-        image = mt.getFinal()
-        if image is not None:
-            cv2.imshow("asd", image)
-            fps = 1/(time.time() - t)
-            print("fps: " + str(int(fps)))
+connection, address = serversocket.accept()
+buf = connection.recv(4096)
+if len(buf) > 0:
+    print buf
+serversocket.close()
+
+#ast.literal_eval("{'1': 2}")
+
+WIDTH = 432
+HEIGHT = 368
+FRAMERATE = 20l
+ROTATION = 180
+camFeed = CameraFeed(WIDTH, HEIGHT, ROTATION, FRAMERATE)
+#print(self.__destinationFolder[7:])
+mt = MotionTracking(camFeed, None)
+t = time.time()
+while True:
+    image = mt.getFinal()
+    if image is not None:
+        cv2.imshow("asd", image)
+        fps = 1/(time.time() - t)
+        print("fps: " + str(int(fps)))
 
 
