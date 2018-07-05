d28e9d69282b684aa6c3decfed0b33324dffd048
lorihompoth <lorihompoth@gmail.com>
2018-07-05 09:56:38 +0000

    main assmebles dictionary

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ from cameraFeed.CameraFeed import CameraFeed
 import cv2
 import time
 import socket
+import ast
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('localhost', 8089))
-serversocket.listen(1)
 
-connection, address = serversocket.accept()
-buf = connection.recv(4096)
-if len(buf) > 0:
-    print buf
-serversocket.close()
-time.sleep(1)
-#ast.literal_eval("{'1': 2}")
+def receiveConfigurables():
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind(('localhost', 8089))
+    serversocket.listen(1)
 
+    connection, address = serversocket.accept()
+    buf = connection.recv(4096)
+    if len(buf) > 0:
+        print buf
+    serversocket.close()
+    return ast.literal_eval(buf)
+
+
+configurables = receiveConfigurables()
+print(configurables)
 WIDTH = 432
 HEIGHT = 368
 FRAMERATE = 20
@@ -25,10 +30,11 @@ camFeed = CameraFeed(WIDTH, HEIGHT, ROTATION, FRAMERATE)
 mt = MotionTracking(camFeed, None)
 t = time.time()
 while True:
+    input("type a number: ")
     image = mt.getFinal()
     if image is not None:
         cv2.imshow("asd", image)
         fps = 1/(time.time() - t)
-        print("fps: " + str(int(fps)))
+        #print("fps: " + str(int(fps)))
 
 
